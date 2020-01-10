2761c3c48c6e06decc8cd396313354ca3b716227
Guillaume PRIGENT <guillaume.prigent92@gmail.com>
2020-01-10 22:26:02 +0100

    Ajout popUp + lien API

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
     <Map></Map>
+    <popUp></popUp>
   </div>
 </template>
 
 <script>
+/* eslint no-console: "off" */
 import Map from "./components/Map";
+import popUp from "./components/popUp";
+import axios from "axios";
 
 export default {
   name: "App",
-  // el: "#togglerLightMode",
   components: {
-    Map
+    Map,
+    popUp
+  },
+  data() {
+    return {
+      pingsAPI: null
+    };
+  },
+  mounted() {
+    // Charge cette partie lorsque App est entièrement chargé
+    axios
+      .get("https://ionia-server.herokuapp.com/api/v1/events")
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 };
 </script>
