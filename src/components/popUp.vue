b1ca73c760697f97ce5675475d2a3de28cf43cfe
Guillaume PRIGENT <guillaume.prigent92@gmail.com>
2020-01-10 23:31:14 +0100

    Card update

--- src/components/popUp.vue
+++ src/components/popUp.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="popUp" v-show="display">
+  <div class="popUp">
     <div class="containerImg">
       <img src="../../src/assets/LyonEsport.jpg" alt="" />
     </div>
     <h2 class="title">Lyon Esport</h2>
     <div class="desc">
-      <p>
+      <p class="boldText">Description :</p>
+      <p style="margin-top: -8px">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus quod, voluptatibus ex pariatur totam eius laborum sunt minima maxime provident fugit,
-        expedita doloremque. Dolores qui, vitae consequuntur ullam ess Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus quod, voluptatibus ex
-        pariatur totam eius laborum sunt minima maxime provident fugit, expedita doloremque. Dolores qui, vitae consequuntur ullam ess Lorem, ipsum dolor sit
-        amet consectetur adipisicing elit. Delectus quod, voluptatibus ex pariatur totam eius laborum sunt minima maxime provident fugit, expedita doloremque.
-        Dolores qui, vitae consequuntur ullam esse excepturi.
+        expedita doloremque. Dolores qui, vitae consequuntur ullam ess Lorem.
+      </p>
+      <p class="boldText">Adresse :</p>
+      <p style="margin-top: -8px">
+        Résultats de recherche 2 Rue du Professeur Charles Appleton, 69007 Lyon
+      </p>
+      <p class="boldText">Horaires :</p>
+      <p style="margin-top: -8px">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus quod, voluptatibus ex pariatur totam eius laborum sunt minima maxime provident fugit,
+        expedita doloremque. Dolores qui, vitae consequuntur ullam ess Lorem.
+      </p>
+      <p class="boldText">Jeux :</p>
+      <p style="margin-top: -8px">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus quod, voluptatibus ex pariatur totam eius laborum sunt minima maxime provident fugit,
+        expedita doloremque. Dolores qui, vitae consequuntur ullam ess Lorem.
       </p>
     </div>
-    <button class="btn" v-on:click="display = false">En savoir plus</button>
+    <button class="btn">En savoir plus</button>
   </div>
 </template>
+
+<script></script>
